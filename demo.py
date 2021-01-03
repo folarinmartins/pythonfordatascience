a186bc91dda47b1e72bd16cd469129b315d8133e
Folarin Martins <folarin@engineer.com>
2021-01-03 01:35:55 +0100

    full repo merge

--- demo.py
+++ demo.py
@@ -1,3 +1,4 @@
+import pandas as pd
 class TestClass(object):
 	def __init__(self):
 		pass
@@ -30,8 +31,39 @@ class TestClass(object):
 			logFile.write('Here is line 1\n')
 			logFile.write('And here is line 2\n')
 			logFile.writelines(['Here is Line 3\n','Here is line 4\n','And line 5\n'])
+	def usePandas(self):
+		df = pd.read_csv('./export.csv')
+		set = {'name':['Akin','Adebayo','Debola'],'age':[10,20,30],'gender':['m','f','o']}
+		df2 = pd.DataFrame(set)
+
+		print(df[['2.5','2.1'][0]])
+		# print(df.iloc[0].unique())
+		# print(df['2.5'].unique())
+		# print(df['2.1']>=1.1)
+		# print(df[df['2.1']>=1.1])
+		# print(df['2.5'][df['2.1']>=1.1])
+		# df.to_csv('filtered.csv')
+		# print(df[['2.1','2.5']])
+
+		# print(df2.iloc[:,:])
+		# print(df2.loc[0:1,'name':'age'])
+
+		# print(df.head())		
+		
+	def useNumpy(self):
+		import numpy as np
+		import matplotlib.pyplot as plt
+		a = np.array([1,2,3,5,7,10,15,21])
+		x = np.linspace(0,np.pi*2,100)
+		y = np.sin(x)
+		plt.plot(x,y)
+		# print(type(a))
+		# print(a.dtype)
+		# print(a)
 test = TestClass()
-test.writeLog()
+test.useNumpy()
+
+
 # print(test.divide(5.4,0))
 
 # err5 = sum([abs(avg5[index] - sol5[index])for index in range(len(avg5))])
